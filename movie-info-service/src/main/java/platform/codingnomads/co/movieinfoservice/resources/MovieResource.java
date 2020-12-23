d3e02b1f9cce3a1741933ecc17fdd605c3e6b067
mash <mash>
2020-12-23 19:12:37 +0600

    update

--- movie-info-service/src/main/java/platform/codingnomads/co/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/platform/codingnomads/co/movieinfoservice/resources/MovieResource.java
@@ -36,9 +36,9 @@ public class MovieResource {
     @GetMapping
     public List<MovieInfoSummary> fetchAllMovieSummaryList(){
         return Arrays.asList(
-                new MovieInfoSummary("1", "Titanic", ""),
-                new MovieInfoSummary("2", "Avatar", ""),
-                new MovieInfoSummary("3", "Forest Grump", "")
+                new MovieInfoSummary("1", "Titanic", " \"Titanic\" is an epic, action-packed romance set against the ill-fated maiden voyage of the R.M.S. Titanic; the pride and joy of the White Star Line and, at the time, the largest moving object ever built. She was the most luxurious liner of her era -- the \"ship of dreams\" -- which ultimately carried over 1,500 people to their death in the ice cold waters of the North Atlantic in the early hours of April 15, 1912."),
+                new MovieInfoSummary("2", "Avatar", "On the lush alien world of Pandora live the Na'vi, beings who appear primitive but are highly evolved. Because the planet's environment is poisonous, human/Na'vi hybrids, called Avatars, must link to human minds to allow for free movement on Pandora. Jake Sully (Sam Worthington), a paralyzed former Marine, becomes mobile again through one such Avatar and falls in love with a Na'vi woman (Zoe Saldana). As a bond with her grows, he is drawn into a battle for the survival of her world."),
+                new MovieInfoSummary("3", "Forrest Gump", "Slow-witted Forrest Gump (Tom Hanks) has never thought of himself as disadvantaged, and thanks to his supportive mother (Sally Field), he leads anything but a restricted life. Whether dominating on the gridiron as a college football star, fighting in Vietnam or captaining a shrimp boat, Forrest inspires people with his childlike optimism. But one person Forrest cares about most may be the most difficult to save -- his childhood love, the sweet but troubled Jenny (Robin Wright).")
         );
     }
 }
